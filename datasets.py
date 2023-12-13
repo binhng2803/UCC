74504120ea63e81801b1d143ff7787ad8c555c9f
binhng2803 <binhng2803@gmail.com>
2023-12-13 20:45:15 +0700

    update transform

--- datasets.py
+++ datasets.py
@@ -46,7 +46,7 @@ class SegDataModule(pl.LightningDataModule):
             split="train",
             transform=A.Compose([A.RandomCrop(380, 380),
                                 A.RandomRotate90(p=0.5),
-                                # A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2), contrast_limit=(-0.2, 0.2), p=0.5),
+                                A.RandomBrightnessContrast(brightness_limit=(0.5, 0.99), contrast_limit=(-0.35, -0.25), p=0.7),
                                 # A.RandomGamma(gamma_limit=(80, 120), p=0.5),
                                 # A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
                                 A.Normalize(mean=[0.285, 0.656, 0.206], std=[0.229, 0.224, 0.225]), 
