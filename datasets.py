2b8d9eaf9e9441c635afd46e561919206d40dc47
binhng2803 <binhng2803@gmail.com>
2023-12-13 21:01:56 +0700

    update transform

--- datasets.py
+++ datasets.py
@@ -49,7 +49,7 @@ class SegDataModule(pl.LightningDataModule):
                                 A.RandomBrightnessContrast(brightness_limit=(0.5, 0.99), contrast_limit=(-0.35, -0.25), p=0.7),
                                 # A.RandomGamma(gamma_limit=(80, 120), p=0.5),
                                 # A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=20, p=0.5),
-                                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
+                                # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
                                 ToTensorV2(),]),
         )
         self.valid = SegDataset(
@@ -57,7 +57,7 @@ class SegDataModule(pl.LightningDataModule):
             phase=self.phase,
             split="valid",
             transform=A.Compose([A.RandomBrightnessContrast(brightness_limit=(0.99, 0.99), contrast_limit=(-0.3, -0.3), p=1.),
-                                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
+                                # A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
                                 ToTensorV2(),]),
         )
 
